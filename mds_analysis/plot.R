fd511e486db8a36a3ab52b70a854d4a971057a29
esohkevin <esohkevin4@gmail.com>
2019-12-08 21:27:28 +0300

    update files

--- mds_analysis/plot.R
+++ mds_analysis/plot.R
@@ -1,12 +1,16 @@
 #!/usr/bin/Rscript
 
 args <- commandArgs(TRUE)
-p <- args[1]
-t <- args[2]
-p <- args[3]
-d <- args[4]
+pe <- args[1]
+te <- args[2]
+pr <- args[3]
+de <- args[4]
+ra <- args[5]
+rb <- args[6]
+ga <- args[7]
+gb <- args[8]
 
-mds_plot <- function(pe=potential_file,
+engy_plot <- function(pe=potential_file,
 		     te=temperature_file,
 		     pr=pressure_file,
 		     de=density_file) {
@@ -25,39 +29,80 @@ mds_plot <- function(pe=potential_file,
    d <- read.table(de, h=F)
    colnames(d) <- c("Time_ps","den")
    #d$den_10ps <- (d$den)/10
-   
+
    png("grom_energies.png", height=10, width=10, units = "in", points=10, res=200)
    par(mfrow=c(2,2))
-   plot(e$Time_ps, e$Energy_kjmol_1, type="l", 
-        main="Potential Energy: 1AKI, NVT Equilibration", 
-        xlab="Time (ps)", 
-        ylab="Potential Energy (kJ/mol)",
-        col="red")
-   #dev.off()
-   
-   plot(t$Time_ps, t$K, type="l", 
-        main="Temperature: 1AKI, NVT Equilibration", 
-        xlab="Time (ps)", 
-        ylab="Temperature (K)",
-        col = "black")
-   abline(h=t_avg, lty=2)
-   
-   plot(p$Time_ps, p$bar, type="l",
-        main="Pressure: 1AKI, NPT Equilibration",
-        xlab="Time (ps)",
-        ylab="Pressure (bar)",
-        col="black")
-   #lines(p$Time_ps, p$bar_10ps, col="red")
-   
-   plot(d$Time_ps, d$den, type="l",
-        main="Density: 1AKI, NPT Equilibration",
-        xlab="Time (ps)",
-        ylab="Density (kg/m^3)",
-        col="black")
-   #lines(d$Time_ps, d$den_10ps, col="red")
-   
+      plot(e$Time_ps, e$Energy_kjmol_1, type="l", 
+           main="Potential Energy: 1AKI, NVT Equilibration", 
+           xlab="Time (ps)", 
+           ylab="Potential Energy (kJ/mol)",
+           col="red")
+      #dev.off()
+      
+      plot(t$Time_ps, t$K, type="l", 
+           main="Temperature: 1AKI, NVT Equilibration", 
+           xlab="Time (ps)", 
+           ylab="Temperature (K)",
+           col = "black")
+      abline(h=t_avg, lty=2)
+      
+      plot(p$Time_ps, p$bar, type="l",
+           main="Pressure: 1AKI, NPT Equilibration",
+           xlab="Time (ps)",
+           ylab="Pressure (bar)",
+           col="black")
+      #lines(p$Time_ps, p$bar_10ps, col="red")
+      
+      plot(d$Time_ps, d$den, type="l",
+           main="Density: 1AKI, NPT Equilibration",
+           xlab="Time (ps)",
+           ylab="Density (kg/m^3)",
+           col="black")
+      #lines(d$Time_ps, d$den_10ps, col="red")
    dev.off()
 
 }
 
-mds_plot(pe=p,te=t,pr=p,de=d)
+ansis_plot <- function(rmsda=first_rmsd_file,
+		       rmsdb=second_rmsd_file, 
+		       gyra=first_gyration_file,
+		       gyrb=second_gyration_file) {
+
+   ra <- read.table(rmsda, h=F)
+   colnames(ra) <- c("time","rmsd")
+
+   rb <- read.table(rmsdb, h=F)
+   colnames(rb) <- c("time","rmsd")
+
+   ga <- read.table(gyra, h=F)
+   colnames(ga) <- c("time","Rt","Rx","Ry","Rz")
+   ga$t_ns <- ga$time/1000
+
+   gb <- read.table(gyrb, h=F)
+   colnames(gb) <- c("time","Rt","Rx","Ry","Rz")
+   gb$t_ns <- gb$time/1000
+
+
+   png("grom_analysis.png", height=10, width=6, units = "in", points=10, res=200)
+   par(mfrow=c(2,1))
+      plot(ra$time, ra$rmsd, 
+           main="RMSD",
+      	type="l",
+      	xlab="Time (ns)",
+      	ylab="RMSD (nm)")
+      lines(rb$time, rb$rmsd,col="red")
+      legend("bottomright", legend=c("Mutant","Wild Type"), col=c("black","red"),pch="-")
+
+      plot(ga$t_ns, ga$Rt,
+           main="Radius of Gyration",
+           type="l",
+           xlab="Time (ns)",
+           ylab="Rg (nm)",
+	   ylim=c(1.30, 1.50))
+      lines(gb$t_ns, gb$Rt,col="red")
+   dev.off()
+}
+
+
+engy_plot(pe=pe,te=te,pr=pr,de=de)
+ansis_plot(rmsda=ra, rmsdb=rb, gyra=ga, gyrb=gb)
--- mds_analysis/script.sh
+++ mds_analysis/script.sh
@@ -9,10 +9,23 @@ if [[ $# != 1 ]]; then
    echo """
    Usage: gromMDS <pdb-file>
    """
-elif [[ $fe != "pdb" ]]; then
+elif [[ $# == 1 && $fe != "pdb" ]]; then
    echo "The input file is not a PDB file! Your file must end with .pdb"
 
 else
+
+# Initialize pbd input & output/intermediate filenames
+fb="${f/.pdb/}" # input base
+fc="${f/.pdb/_clean.pdb}" # clean file - water removed
+fp="${f/.pdb/_processed.gro}" # processed clean file
+fn="${f/.pdb/_newbox.gro}" # box file
+fs="${f/.pdb/_solv.gro}" # solvated file
+fsi="${f/.pdb/_solv_ions.gro}" # solvated file with ions added
+Epe="${f/.pdb/_potential.xvg}"; Epet="${Epe/.xvg/.txt}" # poten ener out files
+Et="${f/.pdb/_temperature.xvg}"; Ett="${Et/.xvg/.txt}" # temp out files
+Epr="${f/.pdb/_pressure.xvg}"; Eprt="${Epr/.xvg/.txt}" # press out files
+Ed="${f/.pdb/_density.xvg}"; Edt="${Ed/.xvg/.txt}" # density out files
+cpt="$fb.cpt" # checkpoint file
 
 # Functions for generating parameter files
 writeIonMdp() {
@@ -56,8 +69,8 @@ pbc             = xyz       ; Periodic Boundary Conditions in all 3 dimensions
 }
 
 writeNvtMdp() {
-echo """
-title                   = OPLS Lysozyme NVT equilibration
+echo -e """
+title                   = OPLS $fb NVT equilibration
 define                  = -DPOSRES  ; position restrain the protein
 ; Run parameters
 integrator              = md        ; leap-frog integrator
@@ -102,8 +115,8 @@ gen_seed                = -1        ; generate a random seed
 }
 
 writeNptMdp() {
-	echo """
-title                   = OPLS Lysozyme NPT equilibration
+	echo -e """
+title                   = OPLS $fb NPT equilibration
 define                  = -DPOSRES  ; position restrain the protein
 ; Run parameters
 integrator              = md        ; leap-frog integrator
@@ -151,8 +164,8 @@ gen_vel                 = no        ; Velocity generation is off
 }
 
 writeMdMdp() {
-	echo """
-title                   = OPLS Lysozyme NPT equilibration
+	echo -e """
+title                   = OPLS $fb NPT equilibration
 ; Run parameters
 integrator              = md        ; leap-frog integrator
 nsteps                  = 500000    ; 2 * 500000 = 1000 ps (1 ns)
@@ -201,27 +214,86 @@ gen_vel                 = no        ; Velocity generation is off
 	"""
 }
 
-# Generate Parameter Files
-writeIonMdp > ions.mdp
-writeMinimMdp > minim.mdp
-writeNvtMdp > nvt.mdp
-writeNptMdp > npt.mdp
-writeMdMdp > md.mdp
+# Generate Molecular Dynamic Parameter Files
+writeIonMdp > ions.mdp # for solvation
+writeMinimMdp > minim.mdp # for energy minimization
+writeNvtMdp > nvt.mdp # for NVT equilibration
+writeNptMdp > npt.mdp # for NPT equilibration
+writeMdMdp > md.mdp # for production MD
 
-# Initialize pbd input & output/intermediate filenames
-fc="${f/.pdb/_clean.pdb}"
-fp="${f/.pdb/_processed.gro}"
-fn="${f/.pdb/_newbox.gro}"
-fs="${f/.pdb/_solv.gro}"
-fsi="${f/.pdb/_solv_ions.gro}"
-Epe="${f/.pdb/_potential.xvg}"; Epet="${Epe/.xvg/.txt}"
-Et="${f/.pdb/_temperature.xvg}"; Ett="${Et/.xvg/.txt}"
-Epr="${f/.pdb/_pressure.xvg}"; Eprt="${Epr/.xvg/.txt}"
-Ed="${f/.pdb/_density.xvg}"; Edt="${Ed/.xvg/.txt}"
+# # Remove water molecules (crystal)
+# grep -v "HOH" $f > $fc
+
+# # Create required files; topology, position restraint, post-processed structure
+# gmx pdb2gmx -f $fc -o $fp -water spce
+# 
+# # Define unit cell & add solvent
+# gmx editconf -f $fp -o $fn -c -d 1.0 -bt cubic
+# gmx solvate -cp $fn -cs spc216.gro -o $fs -p topol.top
+# gmx grompp -f ions.mdp -c $fs -p topol.top -o ions.tpr
+# gmx genion -s ions.tpr -o $fsi -p topol.top -pname NA -nname CL -neutral
+# 
+# # Energy minimization
+# gmx grompp -f minim.mdp -c $fsi -p topol.top -o em.tpr
+# gmx mdrun -v -deffnm em
+# gmx energy -f em.edr -o $Epe
+# grep -v -e "#" -e "@" $Epe > $Epet
+# 
+# # Equilibration Phase 1: NVT (Energy and Temperature)
+# gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
+# gmx mdrun -v -deffnm nvt
+# gmx energy -f nvt.edr -o $Et
+# grep -v -e "#" -e "@" $Et > $Ett
+# 
+# # Equilibration Phase 2: NPT (Pressure and Density)
+# gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
+# gmx mdrun -v -deffnm npt
+# gmx energy -f npt.edr -o $Epr
+# grep -v -e "#" -e "@" $Epr > $Eprt
+# gmx energy -f npt.edr -o $Ed
+# grep -v -e "#" -e "@" $Ed > $Edt
+# 
+# # Run Production MD
+# #gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr
+# #gmx mdrun -v -deffnm md_0_1
+# 
+# # Generate plots
+# Rscript ~/Git/NTD-MANE/mds_analysis/plot.R $Epet $Ett $Eprt $Edt
+
+qsub_gen() {
+echo -e """
+#!/bin/bash
+#PBS -l select=10:ncpus=24:mpiprocs=24
+#PBS -l walltime=96:00:00
+#PBS -q smp
+#PBS -m abe
+#PBS -P CBBI1243
+#PBS -o /mnt/lustre/groups/CBBI1243/KEVIN/mds/stdout.txt
+#PBS -e /mnt/lustre/groups/CBBI1243/KEVIN/mds/stderr.txt
+#PBS -N Gromacs_$fb
+#PBS -M kevin.esoh@students.jkuat.ac.ke
+#PBS -mb
+
+#MODULEPATH=/opt/gridware/bioinformatics/modules:\$MODULEPATH
+#source /etc/profile.d/modules.sh
+
+### # module add/load # ###
+module add chpc/BIOMODULES
+module load gromacs/5.1.4-openmpi_1.10.2-intel16.0.1
+module load R/3.6.0-gcc7.2.0
+
+OMP_NUM_THREADS=1
+
+NP=\`cat \${PBS_NODEFILE} | wc -l\`
+
+mdr=\"gmx_mpi mdrun\"
+#ARGS=\"-s X -deffnm Y\"
+
+cd /mnt/lustre/groups/CBBI1243/KEVIN/mds/
+#mpirun -np \${NP} -machinefile \${PBS_NODEFILE} \${mdr} \${ARGS}
 
 # Remove water molecules (crystal)
-grep -v "HOH" $f > $fc
-
+grep -v \"HOH\" $f > $fc
 
 # Create required files; topology, position restraint, post-processed structure
 gmx pdb2gmx -f $fc -o $fp -water spce
@@ -234,30 +306,45 @@ gmx genion -s ions.tpr -o $fsi -p topol.top -pname NA -nname CL -neutral
 
 # Energy minimization
 gmx grompp -f minim.mdp -c $fsi -p topol.top -o em.tpr
-gmx mdrun -v -deffnm em
+time mpirun -np ${NP} -machinefile ${PBS_NODEFILE} ${mdr} -v -deffnm em #gmx mdrun
 gmx energy -f em.edr -o $Epe
-grep -v -e "#" -e "@" $Epe > $Epet
+grep -v -e \"#\" -e \"@\" $Epe > $Epet
 
 # Equilibration Phase 1: NVT (Energy and Temperature)
 gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
-gmx mdrun -deffnm nvt
+time mpirun -np ${NP} -machinefile ${PBS_NODEFILE} ${mdr} -v -deffnm nvt #gmx mdrun
 gmx energy -f nvt.edr -o $Et
-grep -v -e "#" -e "@" $Et > $Ett
+grep -v -e \"#\" -e \"@\" $Et > $Ett
 
 # Equilibration Phase 2: NPT (Pressure and Density)
 gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
-gmx mdrun -v -deffnm npt
+time mpirun -np ${NP} -machinefile ${PBS_NODEFILE} ${mdr} -v -deffnm npt #gmx mdrun
 gmx energy -f npt.edr -o $Epr
-grep -v -e "#" -e "@" $Epr > $Eprt
+grep -v -e \"#\" -e \"@\" $Epr > $Eprt
 gmx energy -f npt.edr -o $Ed
-grep -v -e "#" -e "@" $Ed > $Edt
+grep -v -e \"#\" -e \"@\" $Ed > $Edt
 
 # Run Production MD
 #gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr
-#gmx mdrun -v -deffnm md_0_1
+#time mpirun -np ${NP} -machinefile ${PBS_NODEFILE} ${mdr} -v -deffnm md_0_1 #gmx mdrun
+
+# Analysis
+#gmx trjconv -s md_0_1.tpr -f md_0_1.xtc -o md_0_1_noPBC.xtc -pbc mol -center # enter 1 0 on prompt
+#gmx rms -s md_0_1.tpr -f md_0_1_noPBC.xtc -o rmsd.xvg -tu ns # enter 4 4 on prompt
+#grep -v -e \"#\" -e \"@\" rmsd.xvg > rmsd.txt
+
+#gmx rms -s em.tpr -f md_0_1_noPBC.xtc -o rmsd_xtal.xvg -tu ns # for crystal structure: 4 4 on prompt
+#grep -v -e \"#\" -e \"@\" rmsd_xtal.xvg > rmsd_xtal.txt
+
+#gmx gyrate -s md_0_1.tpr -f md_0_1_noPBC.xtc -o gyrate.xvg # enter 1 on prompt
+#grep -v -e \"#\" -e \"@\" gyrate.xvg > gyrate.txt
 
 # Generate plots
-Rscript ~/Git/NTD-MANE/mds_analysis/plot.R $Epet $Ett $Eprt $Edt
-
+Rscript /mnt/lustre/groups/CBBI1243/KEVIN/mds/plot.R $Epet $Ett $Eprt $Edt
+"""
+}
+qsub_gen > $fb.qsub
+sleep 1
+echo -e "\n Gromacs job created! Please submit with 'qsub $fb.qsub'\n"
 fi
 }
